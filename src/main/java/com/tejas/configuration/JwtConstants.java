bb87e20e56cb5b86a0bb700ebbd02df987f7db9e
Tejas Patne <tejaspatne18@gmail.com>
2026-08-14 21:07:34 +0530

    setup

---
+++ src/main/java/com/tejas/configuration/JwtConstants.java
@@ -0,0 +1,6 @@
+package com.tejas.configuration;
+
+public class JwtConstants {
+    public static final String JWT_SECRET = "i38n93r9fniefw903vdnnksf9080vdoie9938fnbnaoelb044";
+    public static final String JWT_HEADER = "Authorization";
+}

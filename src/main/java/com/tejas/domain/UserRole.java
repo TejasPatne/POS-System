75c32e14c863d018299901b4e6084ed046427360
Tejas Patne <tejaspatne18@gmail.com>
2026-08-15 08:20:39 +0530

    Add custom user implementation

---
+++ src/main/java/com/tejas/domain/UserRole.java
@@ -0,0 +1,8 @@
+package com.tejas.domain;
+
+public enum UserRole {
+    ROLE_USER, ROLES_ADMIN,
+    ROLE_CASHIER,
+    ROLE_BRANCH_MANAGER,
+    ROLE_STORE_MANAGER
+}
---
+++ src/main/java/com/tejas/model/User.java
@@ -0,0 +1,38 @@
+package com.tejas.model;
+
+import com.tejas.domain.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class User {
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    @Email(message = "Invalid email address")
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    private String phone;
+
+    @Column(nullable = false)
+    private UserRole role;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime lastLogin;
+}
